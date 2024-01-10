a1eeb953acded999407b18ea13c402d20f5b8c85
[Fatih Hakan Direk] <[fatihhakandirek@gmail.com]>
2024-01-10 21:34:38 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProductProject.Models;
+
+namespace ProductProject.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index(string searchString, string category)
+    {
+        var products = Repository.Products;
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            ViewBag.searchString = searchString;
+            products = products.Where(p => p.Name.ToLower().Trim().Contains(searchString)).ToList();
+        }
+
+        // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
+
+        var model = new ProductViewModel{
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
+
+        if (!String.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+        }
+
+        return View(model);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product model)
+    {
+        ViewBag.Categories = Repository.Categories;
+        return View();
+    }
+
+}
